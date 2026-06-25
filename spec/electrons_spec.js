12ff48f4341df62d0d7a36bfb130557cd2fe3d96
Bianca Amberger <bianca@amberger.at>
2026-06-25 23:46:17 +0200

    3 RED: failing test example 2,2,3,3 - BIANCA

--- spec/electrons_spec.js
+++ spec/electrons_spec.js
@@ -10,5 +10,9 @@ describe('Electrons Around The Cores', function() {
 	it('should return 6 for [1,2,3,4,5]', function() {
         expect(game.calculate([1,2,3,4,5])).toEqual(6);
     });
+
+    it('should return 4 for [2,2,3,3]', function() {
+        expect(game.calculate([2,2,3,3])).toEqual(4);
+    });
 });
 
