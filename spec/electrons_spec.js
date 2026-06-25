33c2ff2cf6aebc9f1a0d24a02683ad1a90bf1497
Mariella Pranjic <mariellap.2005@icloud.com>
2026-06-25 23:30:32 +0200

    2 RED: failing test example [1,2,3,4,5] - MARIELLA

--- spec/electrons_spec.js
+++ spec/electrons_spec.js
@@ -6,4 +6,9 @@ describe('Electrons Around The Cores', function() {
     it('should return 0 for an empty array', function() {
         expect(game.calculate([])).toEqual(0);
     });
+
+	it('should return 6 for [1,2,3,4,5]', function() {
+        expect(game.calculate([1,2,3,4,5])).toEqual(6);
+    });
 });
+
