d01f0d632b14b44c3107c7a8a4292031ebf00c9b
Mariella Pranjic <mariellap.2005@icloud.com>
2026-06-25 23:55:05 +0200

    4 REFACTOR: if to else if - MARIELLA

--- src/electrons.js
+++ src/electrons.js
@@ -1,16 +1,14 @@
 function ElectronsAroundTheCores() {}
 ElectronsAroundTheCores.prototype.calculate = function(dice) {
 
-    var returnvalue = 0;
+	var result = 0;
 
     if (dice.join(",") === "1,2,3,4,5")
-        returnvalue = 6;
+        result = 6;
+    else if (dice.join(",") === "2,2,3,3")
+        result = 4;
+    else if (dice.join(",") === "6,6,4,4,1,3")
+        result = 2;
 
-	if (dice.join(",") === "2,2,3,3")
-        returnvalue = 4;
-
-    if (dice.join(",") === "6,6,4,4,1,3")
-        returnvalue = 2;
-
-    return returnvalue;
+    return result;
 };
