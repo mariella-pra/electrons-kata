b795f73b6974abfe2b527d11286d7e18f4f5c45f
Bianca Amberger <bianca@amberger.at>
2026-06-26 00:10:21 +0200

    6 GREEN: random array length 4 - BIANCA

--- src/electrons.js
+++ src/electrons.js
@@ -3,14 +3,13 @@ ElectronsAroundTheCores.prototype.calculate = function(dice) {
 
 	var result = 0;
 
-    if (dice.join(",") === "1,2,3,4,5")
-        result = 6;
-    else if (dice.join(",") === "2,2,3,3")
-        result = 4;
-    else if (dice.join(",") === "6,6,4,4,1,3")
-        result = 2;
-	else if (dice.join(",") === "3,5,3,5,4,2")
-        result = 12;
+    for (var x=0; x<dice.length; x++) {
+        if(dice[x] == 3) {
+            result += 2;
+        } else if(dice[x] == 5) {
+            result += 4;
+        }
+    }
 
     return result;
 };
