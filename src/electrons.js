f6f7ab923ab5efcc7f5521ee75b1e44370b1651a
Mariella Pranjic <mariellap.2005@icloud.com>
2026-06-26 00:00:55 +0200

    5 GREEN: return 12 for example 3,5,3,5,4,2 - MARIELLA

--- src/electrons.js
+++ src/electrons.js
@@ -9,6 +9,8 @@ ElectronsAroundTheCores.prototype.calculate = function(dice) {
         result = 4;
     else if (dice.join(",") === "6,6,4,4,1,3")
         result = 2;
+	else if (dice.join(",") === "3,5,3,5,4,2")
+        result = 12;
 
     return result;
 };
